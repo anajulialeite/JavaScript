7fe6d954cfc089c3d8af72802dccc228d280105d
Ana Júlia Leite <leite.anajulia25@gmail.com>
2024-09-03 16:25:08 -0300

    Add files via upload

---
+++ Aula15/ambiente.js
@@ -0,0 +1,23 @@
+//Estrutura de repetição
+
+var c = 1
+do /*faça*/ {
+    Console.log(`Passo ${c}`)
+    c++ 
+} while /*enquanto*/(c <= 6) 
+
+
+/*var c = 1
+while (c <= 6) {
+    Console.log(`Passo ${c}`)
+    c++ 
+}*/
+
+/*
+    Console.log("Tudo bem?")
+    Console.log("Tudo bem?")
+    Console.log("Tudo bem?")
+    Console.log("Tudo bem?")
+    Console.log("Tudo bem?")
+    Console.log("Tudo bem?") 
+    */
