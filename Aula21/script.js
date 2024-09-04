3aa1759453637cce3dcb5175f85d9bec78658bcd
Ana Júlia Leite <leite.anajulia25@gmail.com>
2024-09-04 16:44:55 -0300

    Update script.js

--- Aula21/script.js
+++ Aula21/script.js
@@ -52,7 +52,7 @@ function finalizar() {
         }
         media = soma / tot
         res.innerHTML = ""
-        res.innerHTML += `<p>Ao todo, temo ${tot} números cadastrados.</p>`
+        res.innerHTML += `<p>Ao todo, temos ${tot} números cadastrados.</p>`
         res.innerHTML += `<p>O maior valor informado foi ${maior}.</p>`
         res.innerHTML += `<p>O menor valor informado foi ${menor}.</p>`
         res.innerHTML += `<p>Somando todos os valores, temos ${soma}.</p>`
