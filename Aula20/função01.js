a79afb7e6bd8dfd80c9eb718cd85674c6c0cf2dd
Ana Júlia Leite <leite.anajulia25@gmail.com>
2024-09-04 14:50:06 -0300

    Add files via upload

---
+++ Aula20/função01.js
@@ -0,0 +1,10 @@
+function parimpar() {
+    if (n % 2 == 0) {
+        return "par!"
+    } else {
+        return "Ímpar!"
+
+    }
+}
+
+console.log(parimpar(223))
---
+++ Aula20/função02.js
@@ -0,0 +1,6 @@
+function soma(n1 = 0, n2 = 0) {
+    return n1 + n2
+}
+
+console.log(soma(2, 5))
+//sem a passagem de 0 ele fica como NAN
