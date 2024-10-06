17db58de3f0d517313ba594aa632b90ec4f4e2e2
Ana Júlia Leite <leite.anajulia25@gmail.com>
2024-10-06 01:30:43 -0300

    Update script.js

--- Aula13/script.js
+++ Aula13/script.js
@@ -2,7 +2,8 @@ function carregar() {
     var msg = window.document.getElementById("msg")
     var img = window.document.getElementById("imagem")
     var data = new Date()
-    var hora = data.getHours()
+    //var hora = 22 caso o usuário queira defibir um horário
+    var hora = data.getHours() //função para ficar igual ao horário do computador
     msg.innerHTML = `Agora são ${hora} horas.`
     if (hora >= 0 && hora < 12) {
         //BOM DIA!!!
